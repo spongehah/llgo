01dee4d1c2eed478ad472031c3d9bc12d1367d35
赵英杰 <2635879218@qq.com>
2024-08-29 11:15:16 +0800

    WIP(x/net/http/client): Extract outwards libuv.Loop and timeout logic

--- x/net/http/_demo/_test/test.go
+++ x/net/http/_demo/_test/test.go
@@ -1,44 +1,12 @@
 package main
 
-import (
-	"fmt"
-	"io"
-	"net/http"
-	"os"
-)
-
 func main() {
-	//url := "http://httpbin.org/post"
-	url := "http://localhost:8080"
-	//filePath := "/Users/spongehah/go/src/llgo/x/net/http/_demo/upload/example.txt" // Replace with your file path
-	filePath := "/Users/spongehah/Downloads/xiaoshuo.txt" // Replace with your file path
+	ch := make(chan struct{}, 1)
 
-	file, err := os.Open(filePath)
-	if err != nil {
-		fmt.Println("Error opening file:", err)
-		return
-	}
-	defer file.Close()
+	close(ch)
 
-	client := &http.Client{}
-	req, err := http.NewRequest("POST", url, file)
-	if err != nil {
-		fmt.Println(err)
-		return
+	select {
+	case <-ch:
+		println(1)
 	}
-	req.Header.Set("expect", "100-continue")
-	resp, err := client.Do(req)
-
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer resp.Body.Close()
-	fmt.Println("Status:", resp.Status)
-	respBody, err := io.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(string(respBody))
 }
